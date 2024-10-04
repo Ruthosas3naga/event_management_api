0fdeb69cc8ae6927a4113786f154f3bfe6cd4a21
osasenaga <edionwe.osasenaga5@gmail.com>
2024-10-04 23:14:37 +0100

    added url path to the url patterns

--- event_management_api/urls.py
+++ event_management_api/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/events_app/', include('posts.urls')),
+    path('api/events_app/', include('events_app.urls')),
 ]
 
