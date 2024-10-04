dab90f702861e5cb21b93ad922768f6bee9a46a0
osasenaga <edionwe.osasenaga5@gmail.com>
2024-10-04 23:24:44 +0100

    Correct the base permission

--- events_app/urls.py
+++ events_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . views import EventDetailView, EventListCreateView, EventRegistrationView, CommentListCreateView
 
 urlpatterns = [
-    path('events/', EventListCreateView.as_view(), related_name='event-list-create'),
+    path('events/', EventListCreateView.as_view()),
     path('events/<int:pk>', EventDetailView.as_view(), related_name='event-detail'),
     path('events/<int:pk>/register/', EventRegistrationView.as_view(), name='event-register'),
     path('events/<int:event-id>/comments/', CommentListCreateView.as_view(), name='event-comments'),
