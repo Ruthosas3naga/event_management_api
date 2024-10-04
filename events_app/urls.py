dd9a3dbf12502a2e46b198db09e82ad6a48c31ce
osasenaga <edionwe.osasenaga5@gmail.com>
2024-10-04 23:35:12 +0100

    Corrected the app urls pattern

--- events_app/urls.py
+++ events_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('events/', EventListCreateView.as_view()),
     path('events/<int:pk>', EventDetailView.as_view(), name='event-detail'),
     path('events/<int:pk>/register/', EventRegistrationView.as_view(), name='event-register'),
-    path('events/<int:event_id>/comments/', CommentListCreateView.as_view(), name='event-comments'),
+    path('events/<int:pk>/comments/', CommentListCreateView.as_view(), name='event-comments'),
 ]
