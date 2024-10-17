0efc0a6271ef325bb6d20c998831de2a23680097
osasenaga <edionwe.osasenaga5@gmail.com>
2024-10-17 16:33:19 +0100

    modified the create_superuser method in account/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,9 @@
 # models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-class CustomUser(AbstractUser):
+class CustomUserManager(BaseUserManager):
     """
     Custom manager to handle user creation.
     """
@@ -23,35 +23,42 @@ class CustomUser(AbstractUser):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, first_name, last_name, password=None):
+    def create_superuser(self, username, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
         # Use create_user instead of calling create_superuser recursively
         user = self.create_user(
             username=username,
             email=self.normalize_email(email),
             password=password,
             first_name=first_name,
-            last_name=last_name
+            last_name=last_name,
+            **extra_fields
         )
-
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
+        # user.is_admin = True
+        # user.is_staff = True
+        # user.is_superuser = True
+        # user.save(using=self._db)
         return user
 
 
+class CustomUser(AbstractUser):
+    """
+    Custom User model extending AbstractUser.
+    """
+    objects = CustomUserManager()  # Use the custom manager
+    # phone_number = models.CharField(max_length=255, blank=True, null=True)
+    # bio = models.TextField(blank=True, null=True)
+    # email = models.EmailField(max_length=255, unique=True)  # Make email unique
+    
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']  # Ensure these fields are required
+    def __str__(self):
+        return self.username
 
-
-# class CustomUser(AbstractUser):
-#     """
-#     Custom User model extending AbstractUser.
-#     """
-#     phone_number = models.CharField(max_length=255, blank=True, null=True)
-#     bio = models.TextField(blank=True, null=True)
-#     email = models.EmailField(max_length=255, unique=True)  # Make email unique
-
-#     objects = CustomUserManager()  # Use the custom manager
-
-#     def __str__(self):
-#         return self.username
 
