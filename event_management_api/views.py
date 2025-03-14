98537148d2b9197a40df19c8e6886cb4236bf8ba
osasenaga <edionwe.osasenaga5@gmail.com>
2025-03-14 19:34:24 +0100

    added links to my view

--- event_management_api/views.py
+++ event_management_api/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the Event Management API!")
+    return HttpResponse("Welcome to the Event Management API!, my endpoints are;"
+    "https://osasenaga.pythonanywhere.com/api/events/", 
+    "https://osasenaga.pythonanywhere.com/api/notiications/",
+    "https://osasenaga.pythonanywhere.com/api/comments/")
